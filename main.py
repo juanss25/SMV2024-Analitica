6d80b6aeee0f8551a9ef122701ec239b5c7d6559
juanss25 <jsantoss@uni.pe>
2024-08-28 10:09:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 data = None
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return jsonify({"hola mundo git"})
     
 # Ruta para subir y procesar el archivo Excel
 @app.route('/upload', methods=['POST'])
